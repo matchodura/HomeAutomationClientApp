593c72a0eafdcc6d9c76af063d62f94dde0607d2
matchodura <69363257+matchodura@users.noreply.github.com>
2022-03-14 20:24:34 +0100

    Add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoggingComponent } from './logging/logging.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dasbhoard', component: DashboardComponent },
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'logging', component: LoggingComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from  '@angular/material/toolbar';
+import { MatIconModule} from  '@angular/material/icon';
+import { MatSidenavModule} from  '@angular/material/sidenav';
+import { MatListModule} from  '@angular/material/list';
+import { MatButtonModule} from  '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoggingComponent } from './logging/logging.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoggingComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
